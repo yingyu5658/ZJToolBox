f8e1b6e930da9cfb28f5ccac959e2d791579d260
yingyu5658 <yingyu5658@outlook.com>
2025-07-24 09:45:42 +0800

    chore: 移除designant.mcz

--- src/Main.js
+++ src/Main.js
@@ -9,7 +9,7 @@ const CLI = require('./user-interface/CLI.js');
 const VERSION = '1.4.1';
 const VERSION_INFO =
     String.raw
-` _____   _ _____           _ ____
+        ` _____   _ _____           _ ____
 |__  /  | |_   _|__   ___ | | __ )  _____  __
   / /_  | | | |/ _ \ / _ \| |  _ \ / _ \ \/ /
  / /| |_| | | | (_) | (_) | | |_) | (_) >  <
@@ -21,4 +21,4 @@ log.log('INFO', 'Main', '程序执行结束');
 const diff = process.hrtime(startTime)
 const nanoseconds = diff[0] * 1e9 + diff[1]
 const time = nanoseconds / 1e6
-log.info( 'Main', `程序结束，总用时：${time} ms`);
+log.info('Main', `程序结束，总用时：${time} ms`);
